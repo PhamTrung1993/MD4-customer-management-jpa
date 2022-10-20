610033e3e240c0cc5995fce514e6d9e2c36e3a86
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-10-21 00:26:42 +0700

    jpa-customer-management

---
+++ src/main/java/com/codegym/repository/customer/ICustomerRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.repository.customer;
+
+import com.codegym.model.Customer;
+import com.codegym.repository.IGeneralRepository;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+
+}
